ed872c538f3bf759d70f21317279df32327fca77
Mariana García <A01378496@itesm.mx>
2019-03-12 22:02:54 -0600

    TransitionScreen y StartScreen terminados

--- core/src/com/mygdx/menudemo/SettingsScreen.java
+++ core/src/com/mygdx/menudemo/SettingsScreen.java
@@ -3,8 +3,17 @@ package com.mygdx.menudemo;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 class SettingsScreen extends ScreenAdapter {
+
+    private final Game game;
+    private static final float WORLD_WIDTH = 1280;
+    private static final float WORLD_HEIGHT = 720;
+
+    private Stage stage;
+
     public SettingsScreen(Game game) {
+        this.game=game;
     }
 }
