4645630ded2d609af226fdc2ffe29c7e9bb388c2
egall <a01745776@itesm.mx>
2019-04-03 18:21:09 -0600

    Se mejoró la clase Kiiw, ya se dibuja en la pantalla

--- core/src/com/mygdx/menudemo/Kiiw.java
+++ core/src/com/mygdx/menudemo/Kiiw.java
@@ -1,5 +1,7 @@
 package com.mygdx.menudemo;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
@@ -7,56 +9,53 @@ import com.badlogic.gdx.math.Circle;
 
 public class Kiiw {
 
-    private static final int TILE_WIDTH = 118;
-    private static final int TILE_HEIGHT = 118;
-    private static final float FRAME_DURATION = 0.25F;
-    private static final float COLLISION_RADIUS = 24f;
-    private static final float RUN_ACCEL = 0.15F;
-    private static final float DISTANCE_BETWEEN_PADS = 13f;
+    private static final float COLLISION_RADIUS=40F;
+    private static final int DISTANCE_BETWEEN_PADS = 20;
+    private static final float FRAME_DURATION = 0.1F;
+    private static final int TILE_WIDTH = 200;
+    private static final int TILE_HEIGHT = 120;
 
-    //private final Animation animation;
     private final Circle collisionCircle;
+    private TextureRegion kiiwTexture;
+    private final Animation animation;
+
+    public static final float RADIUS = COLLISION_RADIUS;
 
     private float x = 0;
     private float y = 0;
     private float animationTimer = 0;
-    private float xSpeed = 0;
-    private TextureRegion kiiwTexture;
 
-    public Kiiw(/*TextureRegion kiiwTexture*/){
-        /*TextureRegion[][] kiiwTextures = new TextureRegion(kiiwTexture).split(TILE_WIDTH, TILE_HEIGHT);
-
-        animation = new Animation(FRAME_DURATION, kiiwTextures[0][0],kiiwTextures[0][1]);
+    public Kiiw(Texture kiiwTexture) {
+        TextureRegion [][] kiiwTextures = new TextureRegion(kiiwTexture).split(TILE_WIDTH, TILE_HEIGHT);
+        animation = new Animation(FRAME_DURATION, kiiwTextures[0][0], kiiwTextures[0][1],kiiwTextures[0][2],kiiwTextures[0][3]);
         animation.setPlayMode(Animation.PlayMode.LOOP);
-*/
-        collisionCircle = new Circle(x,y,COLLISION_RADIUS);
+        collisionCircle = new Circle(x,y, COLLISION_RADIUS);
     }
 
     public void draw(SpriteBatch batch){
-        //TextureRegion kiiwTexture = (TextureRegion) animation.getKeyFrame(animationTimer);
+        TextureRegion kiiwTexture = (TextureRegion) animation.getKeyFrame(animationTimer);
         float textureX = collisionCircle.x - kiiwTexture.getRegionWidth()/2;
         float textureY = collisionCircle.y - kiiwTexture.getRegionHeight()/2;
         batch.draw(kiiwTexture, textureX, textureY);
     }
 
-    public void drawDebug(ShapeRenderer shapeRenderer){
+    public void drawDebug(ShapeRenderer shapeRenderer) {
         shapeRenderer.circle(collisionCircle.x, collisionCircle.y, collisionCircle.radius);
     }
 
-    public void setPosition(float x, float y){
+    public void setPosition(float x, float y) {
         this.x = x;
         this.y = y;
         updateCollisionCircle();
     }
 
+    public void update(float delta) {
+        animationTimer += delta;
+    }
+
     private void updateCollisionCircle() {
         collisionCircle.setX(x);
         collisionCircle.setY(y);
-    }
-
-    public void update(float delta){
-        animationTimer+=delta;
-        xSpeed+= RUN_ACCEL;
     }
 
     public void changePadUp(){
@@ -69,16 +68,19 @@ public class Kiiw {
         setPosition(x, y);
     }
 
+    public float getY() {
+        return y;
+    }
+
     public float getX() {
         return x;
     }
 
-    public float getY() {
-        return y;
+    public float getHeigth() {
+        return COLLISION_RADIUS;
     }
 
     public Circle getCollisionCircle() {
         return collisionCircle;
     }
-
 }
